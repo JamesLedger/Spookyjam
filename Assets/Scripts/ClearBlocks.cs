4af4d5549b9981ed10ac505d6b2ccefdbdf66e7f
James Ledger <Jamesledger99@gmail.com>
2020-10-19 18:44:42 +0100

    Initialising repo with basic handtracking scene

---
+++ Assets/Scripts/ClearBlocks.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearBlocks : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+       
+        var gameObjects = GameObject.FindGameObjectsWithTag("cubee");
+
+        for (var i = 0; i < gameObjects.Length; i++)
+            Destroy(gameObjects[i]);
+
+        Debug.Log("trigger");
+    }
+}
---
+++ Assets/Scripts/ClickSpawn.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickSpawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public bool spawnReady;
+    public bool hasSpawned;
+
+    void Start()
+    {
+        spawnReady = false;
+        hasSpawned = false;
+    }
+
+    public GameObject spawnObject;
+    public Transform spawnPos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GestureDetector detector = GetComponent<GestureDetector>();
+        CurrentHandInfo info = detector.Recognise();
+
+        if (info.rightHandGesture == "ClickBegin")
+        {
+            spawnReady = true;
+            hasSpawned = false;
+        }
+
+        if (info.rightHandGesture == "ClickEnd" && spawnReady == true)
+        {
+            spawnReady = false;
+            hasSpawned = true;
+
+            GameObject tempCube = Instantiate(spawnObject);
+            tempCube.transform.position = spawnPos.position;
+        }
+
+
+    }
+}
