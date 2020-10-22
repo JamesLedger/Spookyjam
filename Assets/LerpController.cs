ed94e9edd53101e8dac30636a313dc979da3a1c6
James Ledger <Jamesledger99@gmail.com>
2020-10-22 11:44:01 +0100

    Adding in art/music asset pack

---
+++ Assets/LerpController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MEC;
+
+public class LerpController : MonoBehaviour
+{
+    private Animator anim;
+    private void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    public void Activate()
+    {
+        anim.SetBool("Up", true);
+    }
+    public void DeActivate()
+    {
+        anim.SetBool("Up", false);
+    }
+    IEnumerator<float> _FlipUp(Quaternion endValue, Quaternion startValue, float time, float duration)
+    {
+        yield return Timing.WaitForOneFrame;
+    }
+    IEnumerator<float> _FlipDown(Quaternion endValue, Quaternion startValue, float time, float duration)
+    {
+        yield return Timing.WaitForOneFrame;
+    }
+
+}
