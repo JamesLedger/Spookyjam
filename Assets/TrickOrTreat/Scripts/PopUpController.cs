3bf2f14f2025e0eaa30f2ca6ab1d4e109fef2674
James Ledger <Jamesledger99@gmail.com>
2020-10-24 14:15:36 +0100

    Starting on chunk system with proceduarally generated props. Also added music

---
+++ Assets/TrickOrTreat/Scripts/PopUpController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpController : MonoBehaviour
+{
+    public Vector3 upAngle = new Vector3(-90f, 0f, 0f);
+    public Vector3 downAngle = new Vector3(-90f, 0f, 0f);
+
+    private Vector3 currentAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentAngle = transform.eulerAngles;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.LogError("shouldPopUp");
+        if (other.tag == "popUp")
+        {
+            currentAngle = new Vector3(Mathf.LerpAngle(currentAngle.x, upAngle.x, Time.deltaTime * 5),0,0);
+            transform.eulerAngles = currentAngle;
+        }
+        else if(other.tag == "popDown")
+        {
+            currentAngle = new Vector3(0, 0, 0);
+            transform.eulerAngles = currentAngle;
+        }
+    }
+}
---
+++ Assets/TrickOrTreat/Scripts/ReplaceChunk.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaceChunk : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject chunkSpawnPoint;
+
+    [SerializeField]
+    public List<GameObject> decorativePropList;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "ResetChunk")
+        {
+            gameObject.transform.position = new Vector3(0,0,70f);
+         //   ResetChunk();
+         //   PopulateChunk();
+        }
+    }
+
+    public int decorationChance;
+
+    private void ResetChunk()
+    {
+        GameObject decorativeProps = gameObject.transform.GetChild(1).gameObject;
+
+        foreach (Transform decorativePropTransform in decorativeProps.transform)
+        {
+            decorationChance = Random.Range(0, 10);
+
+            if (decorationChance > 4)
+            {
+                int selection = Random.Range(0, decorativePropList.Count);
+
+                GameObject decorativeProp = Instantiate(decorativePropList[selection]);
+                decorativeProp.transform.position = decorativePropTransform.position;
+            }
+        }
+       
+    }
+
+    private void PopulateChunk()
+    {
+
+    }
+}
