7bb23b27a8afe924c8c96837076a52fb59cfda8a
ninjafella <52487596+ninjafella@users.noreply.github.com>
2020-10-25 21:14:28 +0000

    Bat sound added as well as a spawner for the bats

---
+++ Assets/Scripts/Bat_movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bat_movement : MonoBehaviour
+{
+    //setting speed
+    [Tooltip("Speed at which you want the bat to move at")]
+    public float speed;
+    [Tooltip("How long after the bat is spawned do you want it to be destoryed.")]
+    public float destroy_timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(destroy());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //moving the bat forwards forever
+        transform.position += new Vector3(0,0,-1) * speed * Time.deltaTime;
+    }
+
+    IEnumerator destroy()
+    {
+        //waits until the destroy timer time has been fulfilled
+        yield return new WaitForSeconds(destroy_timer);
+        //destroys itself
+        Destroy(this.gameObject);
+    }
+}
