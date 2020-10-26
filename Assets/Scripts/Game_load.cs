2e0651e0abd558d483ab8a5a0b5aea6d6c15d35d
ninjafella <52487596+ninjafella@users.noreply.github.com>
2020-10-26 18:21:55 +0000

    You can now start the game with the thumbs up gesture

---
+++ Assets/Scripts/Game_load.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game_load : MonoBehaviour
+{
+    //scene navigation index
+    [SerializeField]
+    private int waitingRoomSceneIndex;
+
+    private GestureDetector gestureDetector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gestureDetector = GameObject.Find("/GestureDetector").GetComponent<GestureDetector>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gestureDetector.Recognise().leftHandGesture == "Ok" || gestureDetector.Recognise().rightHandGesture == "Ok")
+        {
+            // load into waiting room scene
+            SceneManager.LoadScene(waitingRoomSceneIndex);
+        }
+    }
+}
